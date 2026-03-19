9bb3a245a654cc2d228e86df86f35238ba07451a
Avijit Pandit <avijitpanditroll@gmail.com>
2026-03-19 15:42:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",        // tells Next.js to generate static HTML files
+  basePath: "/avijitpandit", // because your repo is NOT username.github.io
 };
 
 export default nextConfig;
